1b0687b3ddcded9240d40b33514c235cbce80cfa
Estefani <stef45689@gmail.com>
2021-08-13 15:50:41 -0500

    HTML, CSS, JS Added

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 // import functions and grab DOM elements
+const touchdown = document.querySelector('#score');
+const counterEl = document.querySelector('#counter');
+
+const touchdown1= document.querySelector('#score1');
+const counterEl1 = document.querySelector('#counter1');
 
 // initialize state
-
+let counter = 0;
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+touchdown.addEventListener('click',()=>{
+    counter = counter + 7; 
+    counterEl.textContent = counter;
+  });
+
+touchdown1.addEventListener('click',()=>{
+  counter = counter + 7; 
+  counterEl1.textContent = counter;
+})
+
